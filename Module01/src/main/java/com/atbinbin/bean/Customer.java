7ba2bfe51e6cc1ab0a77b9d265dde833173cb515
LBB <3290333649@qq.com>
2023-04-10 16:30:41 +0800

    Initial commit

---
+++ Module01/src/main/java/com/atbinbin/bean/Customer.java
@@ -0,0 +1,7 @@
+package com.atbinbin.bean;
+
+public class Customer {
+    public static void main(String[] args) {
+        System.out.println("helloworld!!");
+    }
+}
---
+++ src/main/java/com/atbinbin/java/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.atbinbin.java;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloworld!!");
+        
+    }
+}
