ddc5a01f40e638fd7f609338f994ab8dee993559
LBB <3290333649@qq.com>
2023-04-11 14:31:48 +0800

    TEST0411

---
+++ Module01/src/main/java/com/atbinbin/bean/Test3.java
@@ -0,0 +1,11 @@
+package com.atbinbin.bean;
+
+/**
+ * @author LBB
+ * @create 2023-04-10 18:15
+ */
+public class Test3 {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
